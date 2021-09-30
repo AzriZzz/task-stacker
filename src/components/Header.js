798c4d86af8b31cf25e3c6fc3204a9cd2588ddc1
Muhammad Azri <lenovorosak@gmail.com>
2021-10-01 00:00:24 +0800

    Integrate with JSON-server, Routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
 const Header = ({ onAdd, showAdd }) => {
   const title = "Task Tracker";
+  const location = useLocation();
 
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        color={showAdd ? "red" : "green"}
-        text={showAdd ? "Close" : "Add"}
-        onClick={onAdd}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Hide" : "Show"}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
